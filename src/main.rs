a8ab0093c51c3c56e0e6260b6426f8715c1f2068
Ryan Arana <ryan.arana@hey.com>
2022-04-08 21:38:44 -0700

    Add court

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
         .add_plugin(ShapePlugin)
 
         .add_startup_system(setup_camera)
+        .add_startup_system(setup_court)
         .add_startup_system(setup_ball)
         .add_startup_system(setup_paddles)
         .run();
@@ -22,23 +23,77 @@ fn setup_camera(mut commands: Commands) {
 }
 
 #[derive(Component)]
+struct Court;
+
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+impl Size {
+    fn as_vec2(&self) -> Vec2 {
+        Vec2::new(self.width, self.height)
+    }
+}
+
+fn setup_court(mut commands: Commands, windows: Res<Windows>) {
+    let window = windows.get_primary().unwrap();
+    let ball_radius = ball_radius(window.width());
+    let size = Size {
+        width: window.width() - ball_radius,
+        height: window.height() - ball_radius
+    };
+
+    let shape = shapes::Rectangle {
+        origin: RectangleOrigin::Center,
+        extents: size.as_vec2()
+    };
+
+    commands
+        .spawn_bundle(GeometryBuilder::build_as(
+            &shape,
+            DrawMode::Outlined {
+                fill_mode: FillMode::color(Color::Rgba { alpha: 0.0, red: 0.0, green: 0.0, blue: 0.0 }),
+                outline_mode: StrokeMode::new(Color::WHITE, 10.0),
+            },
+            Transform::default(),
+        ))
+        .insert(size)
+        .insert(Court);
+}
+
+#[derive(Component)]
 struct Ball;
+
+#[derive(Component)]
+struct Velocity {
+    x: f32,
+    y: f32
+}
+
+fn ball_radius(window_width: f32) -> f32 {
+    window_width / SIZE_FACTOR / 1.333
+}
 
 fn setup_ball(mut commands: Commands, windows: Res<Windows>) {
     let window = windows.get_primary().unwrap();
-    let ball_radius = window.width() / SIZE_FACTOR / 1.333;
+    let ball_radius = ball_radius(window.width());
     let shape = shapes::Circle {
         center: Vec2::new(0.0, 0.0),
         radius: ball_radius,
     };
 
-    commands.spawn_bundle(GeometryBuilder::build_as(
-        &shape,
-        DrawMode::Fill {
-            0: FillMode::color(Color::WHITE)
-        },
-        Transform::default()
-    )).insert(Ball);
+    commands
+        .spawn_bundle(GeometryBuilder::build_as(
+            &shape,
+            DrawMode::Fill {
+                0: FillMode::color(Color::WHITE)
+            },
+            Transform::default())
+        )
+        .insert(Ball)
+        .insert(Velocity { x: 10.0, y: 0.0 });
+}
 }
 
 #[derive(Component)]
